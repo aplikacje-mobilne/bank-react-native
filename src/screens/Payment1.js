8f0586367fa754df4413e2f18f149aeddca228aa
wardusV2 <p.anonim88@gmail.com>
2023-12-17 15:57:28 +0100

    payment z bazą

--- src/screens/Payment1.js
+++ src/screens/Payment1.js
@@ -1,63 +1,151 @@
-import React from 'react';
-import { View, Text,TextInput,TouchableOpacity} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, TouchableOpacity, Alert } from 'react-native';
 import { styles } from './styles';
-import { useState } from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome5';
-const Payment1 = ({navigation}) => {
-  
-  const [input1, setInput1] = useState('');
-  const [input2, setInput2] = useState('');
-  const [input3, setInput3] = useState('');
-  const [input4, setInput4] = useState('');
-  const [input5, setInput5] = useState('');
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const Payment1 = ({ navigation }) => {
+  const [dane, setInput1] = useState("");
+  const [nr_rachunku, setInput2] = useState("");
+  const [tytul, setInput3] = useState("");
+  const [opis, setInput4] = useState("");
+  const [kwota, setInput5] = useState("");
+  const [loggedInUser, setLoggedInUser] = useState(null);
+  const [toUserLogin, setUserLogin] = useState(null);
+
+  useEffect(() => {
+    const fetchLoggedInUser = async () => {
+      try {
+        const storedLoggedInUser = await AsyncStorage.getItem('loggedInUser');
+        if (storedLoggedInUser) {
+          const parsedUser = JSON.parse(storedLoggedInUser);
+          const userLogin = parsedUser.login; // Załóżmy, że pole login zawiera login użytkownika
+          setLoggedInUser(userLogin);
+        }
+      } catch (error) {
+        console.error('Błąd podczas pobierania loginu zalogowanego użytkownika:', error);
+      }
+    };
+
+    fetchLoggedInUser();
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`http://192.168.8.179:3001/users?name=${dane}`);
+        if (response.data.length > 0) {
+          const toUserLogin = response.data[0].login; // Załóżmy, że login znajduje się w polu "login" obiektu
+          setUserLogin(toUserLogin); // Ustaw login odbiorcy w stanie komponentu
+
+          // Zapisz login odbiorcy w AsyncStorage
+          await AsyncStorage.setItem('toUserLogin', JSON.stringify({ login: toUserLogin }));
+        }
+      } catch (error) {
+        console.error('Błąd podczas pobierania loginu odbiorcy:', error);
+      }
+    };
+
+    fetchData();
+  }, [dane]);
+
+  const checkName = async (Name) => {
+    try {
+      const response = await axios.get(`http://192.168.8.179:3001/users?name=${Name}`);
+      return response.data.length > 0;
+    } catch (error) {
+      console.error('Błąd podczas sprawdzania odbiorcy:', error);
+      return false;
+    }
+  };
+
+  const checknrkonta = async (Name, Number) => {
+    try {
+      const response = await axios.get(
+        `http://192.168.8.179:3001/users?name=${Name}&nrkonta=${Number}`
+      );
+      return response.data.length > 0;
+    } catch (error) {
+      console.error('Błąd podczas sprawdzania numeru rachunku:', error);
+      return false;
+    }
+  };
+
+  const handleCheck = async () => {
+    const recipientExists = await checkName(dane);
+    if (!recipientExists) {
+      Alert.alert('Błąd', 'Odbiorca o podanym imieniu nie istnieje.');
+      return;
+    }
+
+    const accountNumberMatches = await checknrkonta(dane, nr_rachunku);
+    if (!accountNumberMatches) {
+      Alert.alert('Błąd', 'Numer rachunku nie jest zgodny z odbiorcą.');
+      return;
+    }
+
+    try {
+      await axios.post("http://192.168.8.179:3001/transaction", {
+        loggedInUser,
+        nr_rachunku,
+        toUserLogin,
+        opis,
+        kwota
+      });
+
+      Alert.alert('Przelew udany');
+      navigation.goBack();
+    } catch (error) {
+      console.error('Błąd podczas wysyłania danych:', error);
+    }
+  };
+
   return (
-      <View style={styles.header}>
-          <View style={styles.sectionContainerp1}>
-              <View style={styles.sectionContent}>
-              <Icon name="landmark" size={35}/>
-                  <Text style={styles.sectionText}>Przelew Krajowy</Text>
-              </View>
-          </View>
-          <View style={styles.sectionContainer}>
-         
-          
-          <TextInput
+    <View style={styles.header}>
+      <View style={styles.sectionContainerp1}>
+        <View style={styles.sectionContent}>
+          <Icon name="landmark" size={35} />
+          <Text style={styles.sectionText}>Przelew Krajowy</Text>
+        </View>
+      </View>
+      <View style={styles.sectionContainer}>
+        <TextInput
           style={styles.input}
           placeholder="Imię i nazwisko odbiorcy:"
-          value={input1}
+          value={dane}
           onChangeText={(text) => setInput1(text)}
         />
         <TextInput
           style={styles.input}
           placeholder="Numer rachunku:"
-          value={input2}
+          value={nr_rachunku}
           onChangeText={(text) => setInput2(text)}
         />
         <TextInput
           style={styles.input}
           placeholder="Tytuł:"
-          value={input3}
+          value={tytul}
           onChangeText={(text) => setInput3(text)}
         />
         <TextInput
           style={styles.input}
           placeholder="Opis:"
-          value={input4}
+          value={opis}
           onChangeText={(text) => setInput4(text)}
         />
         <TextInput
           style={styles.input}
           placeholder="Kwota"
-          value={input5}
+          value={kwota}
           onChangeText={(text) => setInput5(text)}
         />
-        <TouchableOpacity style={styles.check}>
+        <TouchableOpacity style={styles.check} onPress={handleCheck}>
           <Text style={styles.pp}>Check</Text>
         </TouchableOpacity>
-          </View>
       </View>
-    );
+    </View>
+  );
 };
-
 
 export default Payment1;
--- src/screens/Payment2.js
+++ src/screens/Payment2.js
@@ -1,14 +1,95 @@
 import React from 'react';
-import { View, Text, StyleSheet,Image,TouchableOpacity,TextInput } from 'react-native';
+import { View, Text, StyleSheet,Image,TouchableOpacity,TextInput,Alert } from 'react-native';
 import { styles } from './styles';
 import { useState } from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
 import Icon from 'react-native-vector-icons/FontAwesome5';
-const Payment2 = () => {
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const Payment2 = ({navigation}) => {
   
-  const [input1, setInput1] = useState('');
-  const [input2, setInput2] = useState('');
-  const [input3, setInput3] = useState('');
-  const [input4, setInput4] = useState('');
+  const [nr_telefonu, setInput1] = useState('');
+  const [tytul, setInput2] = useState('');
+  const [opis, setInput3] = useState('');
+  const [kwota, setInput4] = useState('');
+  const [loggedInUser, setLoggedInUser] = useState(null);
+  const [toUser, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchLoggedInUser = async () => {
+      try {
+        const storedLoggedInUser = await AsyncStorage.getItem('loggedInUser');
+        if (storedLoggedInUser) {
+          const parsedUser = JSON.parse(storedLoggedInUser);
+          const userLogin = parsedUser.login; // Załóżmy, że pole login zawiera login użytkownika
+          setLoggedInUser(userLogin);
+        }
+      } catch (error) {
+        console.error('Błąd podczas pobierania loginu zalogowanego użytkownika:', error);
+      }
+    };
+
+    fetchLoggedInUser();
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`http://192.168.8.179:3001/users?tel=${nr_telefonu}`);
+        if (response.data.length > 0) {
+          const toUser = response.data[0].login; // Załóżmy, że login znajduje się w polu "login" obiektu
+          setUser(toUser); // Ustaw login odbiorcy w stanie komponentu
+
+          // Zapisz login odbiorcy w AsyncStorage
+          await AsyncStorage.setItem('toUserLogin', JSON.stringify({ login: toUser }));
+        }
+      } catch (error) {
+        console.error('Błąd podczas pobierania loginu odbiorcy:', error);
+      }
+    };
+
+    fetchData();
+  }, [nr_telefonu]);
+
+  const checknrtel = async (nrtel) => {
+    // Sprawdzenie, czy istnieje odbiorca o podanym imieniu w bazie users
+    try {
+      const response = await axios.get(`http://192.168.8.179:3001/users?tel=${nrtel}`);
+      return response.data.length > 0;
+    } catch (error) {
+      console.error('Błąd podczas sprawdzania odbiorcy:', error);
+      return false;
+    }
+  };
+  const handleCheck = async () => {
+
+    const recipientExists = await checknrtel(nr_telefonu);
+    if (!recipientExists) {
+      Alert.alert('Błąd', 'Odbiorca o podanym numerze telefonu nie istnieje.');
+      return;
+    }
+
+    try {
+      // Tworzenie obiektu z danymi do wysłania
+      await axios.post("http://192.168.8.179:3001/transaction", {
+        loggedInUser,
+        tytul,
+        toUser,
+        opis,
+        kwota
+      });
+
+      alert('Przelew udany');
+      navigation.goBack();
+      // Przetworzenie odpowiedzi, np. obsługa komunikatu zwrotnego
+      //console.log('Odpowiedź z serwera:', response.data);
+
+    } catch (error) {
+      console.error('Błąd podczas wysyłania danych:', error);
+    }
+  };
+
   return (
     <View style={styles.header}>
         <View style={styles.sectionContainerp1}>
@@ -23,28 +104,28 @@ const Payment2 = () => {
         <TextInput
         style={styles.input}
         placeholder="Numer Telefonu Odbiorcy:"
-        value={input1}
+        value={nr_telefonu}
         onChangeText={(text) => setInput1(text)}
       />
       <TextInput
         style={styles.input}
         placeholder="Tytuł:"
-        value={input2}
+        value={tytul}
         onChangeText={(text) => setInput2(text)}
       />
       <TextInput
         style={styles.input}
         placeholder="Opis:"
-        value={input3}
+        value={opis}
         onChangeText={(text) => setInput3(text)}
       />
       <TextInput
         style={styles.input}
         placeholder="Kwota:"
-        value={input4}
+        value={kwota}
         onChangeText={(text) => setInput4(text)}
       />
-      <TouchableOpacity style={styles.check}>
+      <TouchableOpacity style={styles.check} onPress={handleCheck}>
         <Text style={styles.pp}>Check</Text>
       </TouchableOpacity>
         </View>
