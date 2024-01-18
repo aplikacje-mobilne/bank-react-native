fc94ef9cc6834ae78948675ea811f2443887e128
Jakub H <102822779+Kuba1717@users.noreply.github.com>
2024-01-18 10:50:07 +0100

    Add files via upload
    oferty poprawa

--- src/screens/OfferFirst.js
+++ src/screens/OfferFirst.js
@@ -1,33 +1,32 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Image, Text, StyleSheet } from 'react-native';
 
 const OfferFirst = () => {
   return (
-    
     <View style={styles.container}>
-      
-   
-      
-
       <View style={styles.rectangle}>
-        <Text style={styles.headerText}>Przedstawiamy naszą nową promocję Konta Oszczędnościowego, w której możesz zarobić aż do 5% odsetek przez okres 90 dni. Ta ograniczona czasowo oferta ma na celu pomóc Ci zmaksymalizować potencjał oszczędności.</Text>
-        <Text style={styles.text}>1. Zarabiaj do 8% odsetek od swoich oszczędności przez okres 90 dni.</Text>
-        <Text style={styles.text}>2. Promocja dostępna jest dla nowych środków wpłaconych na konto, z maksymalnym limitem wpłat wynoszącym 500 000 zł.</Text>
-        <Text style={styles.text}>3. Oprocentowanie jest gwarantowane przez cały okres 90 dni.</Text>
-        <Text style={styles.text}>4. Twoje środki pozostają dostępne, zapewniając elastyczność, gdy ich potrzebujesz.</Text>
-        <Text style={styles.text}>5. Promocja dostępna jest przez ograniczony czas, dlatego warto działać szybko, aby skorzystać z tej oferty.</Text>
-        <Text style={styles.text}>6. Odsetki naliczane są codziennie i naliczane na Twoje konto po upływie 90 dni.</Text>
-        <Text style={styles.text}>7. Dochody odsetkowe podlegają standardowym przepisom podatkowym.</Text>
-        <Text style={styles.text}>8. Kontem możesz wygodnie zarządzać poprzez bankowość internetową.</Text>
-        <Text style={styles.text}>9. W okresie promocyjnym nie są pobierane żadne opłaty za prowadzenie konta.</Text>
-        <Text style={styles.text}>10. Obowiązują zasady i warunki, więc pamiętaj o zapoznaniu się z nimi przed otwarciem konta.</Text>
-        <Text style={styles.headerText}>Nie przegap okazji na powiększenie swoich oszczędności dzięki naszej promocji Konta Oszczędnościowego. Zacznij oszczędzać z nami już dziś!</Text>
-
-
+        <Image
+          source={require('./1.png')}
+          style={{ width: 120, height: 120, position: 'absolute', top: 10, right: 10 }}
+        />
+        <Text style={[styles.headerText, { marginRight: 140 }]}>
+          Oszczędzaj nawet na 8% na rachunku oszczędnościowym!
+        </Text>
+        <Text style={[styles.headerText2, { marginRight: 50 }]}>
+          Nie przegap okazji na powiększenie swoich oszczędności dzięki naszej promocji Konta Oszczędnościowego. Zacznij oszczędzać z nami już dziś!
+        </Text>
+        <Text style={[styles.headerText2, { marginRight: 50 }]}>
+        Oprocentowanie jest gwarantowane przez cały okres: 
+        </Text>
+        <Text style={[styles.headerText3, { marginRight: 50 }]}>
+        90 dni!
+        </Text>
+        <Text style={styles.text}>Twoje środki pozostają dostępne, zapewniając elastyczność, gdy ich potrzebujesz.</Text>
+        <Text style={styles.text}>Promocja dostępna jest przez ograniczony czas, dlatego warto działać szybko, aby skorzystać z tej oferty.</Text>
+        <Text style={[styles.headerText4]}>
+        O więcej szczegółów zapytaj w punkcie banku
+        </Text>
       </View>
-
-     
-
     </View>
   );
 };
@@ -39,30 +38,56 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     textAlign: 'left',
   },
+  text: {
+    fontSize: 15, 
+    marginBottom: 5,
+    marginLeft: 15,
+    marginRight: 15,
+  
+  },
   headerText: {
-    fontSize: 15,
+    fontSize: 20,
     fontWeight: 'bold',
-    marginTop: 10, 
-    marginBottom: 5, 
-    marginLeft: 10, 
+    marginTop: 15,
+    marginBottom: 5,
+    marginLeft: 15,
     marginRight: 10,
     textAlign: 'left',
-
   },
-  text: {
-    fontSize: 12, 
+  headerText2: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 15,
     marginBottom: 5,
-
-  
+    marginLeft: 15,
+    marginRight: 10,
+    textAlign: 'left',
+  },
+  headerText3: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    marginTop: 10,
+    marginBottom: 15,
+    marginLeft: 15,
+    marginRight: 10,
+    textAlign: 'center',
+  },
+  headerText4: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 25,
+    marginBottom: 15,
+    marginLeft: 15,
+    marginRight: 10,
+    textAlign: 'center',
   },
   rectangle: {
     backgroundColor: 'white',
-    width: 350, 
-    height: 580, 
+    width: 350,
+    height: 580,
     borderRadius: 8,
     marginVertical: 20,
   },
-
 });
 
 export default OfferFirst;
--- src/screens/OfferSecond.js
+++ src/screens/OfferSecond.js
@@ -1,32 +1,32 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Image, Text, StyleSheet } from 'react-native';
 
-const OfferSecond = () => {
+const OfferFirst = () => {
   return (
-    
     <View style={styles.container}>
-      
-   
-      
-
       <View style={styles.rectangle}>
-        <Text style={styles.headerText}>Przedstawiamy naszą promocję Celu Inwestycyjnego! Dzięki tej ofercie specjalnej masz szansę zarobić aż do 200 zł w postaci bonusów, a wysokość bonusu uzależniona jest od ustawionej przez Ciebie kwoty zlecenia stałego.</Text>
-        <Text style={styles.text}>1. Zakładając zlecenie stałe na swoje inwestycje, możesz otrzymać premie w wysokości do 200 zł.</Text>
-        <Text style={styles.text}>2. Kwota premii wzrasta w zależności od wielkości Twojego zlecenia stałego, nagradzając Twoje zaangażowanie w oszczędzanie i inwestowanie.</Text>
-        <Text style={styles.text}>3. Im większe jest Twoje zlecenie stałe, tym wyższy bonus możesz otrzymać.</Text>
-        <Text style={styles.text}>4. Bonusy zostaną dodane do Twojego konta zgodnie z wybraną kwotą zlecenia stałego.</Text>
-        <Text style={styles.text}>5. Promocja jest dostępna przez ograniczony czas, więc nie przegap okazji, aby zmaksymalizować swoje nagrody.</Text>
-        <Text style={styles.text}>6. Możesz wybrać kwotę zlecenia stałego, która najlepiej odpowiada Twoim celom inwestycyjnym.</Text>
-        <Text style={styles.text}>7. Premie stanowią dodatek do wszelkich potencjalnych zysków lub zysków z inwestycji.</Text>
-        <Text style={styles.text}>8. Nasi doradcy inwestycyjni są do Twojej dyspozycji, aby pomóc Ci w podejmowaniu świadomych decyzji dotyczących Twojego zlecenia stałego.</Text>
-        <Text style={styles.text}>9. Obowiązują zasady i warunki, więc zapoznaj się z nimi, aby w pełni skorzystać z tej promocji.</Text>
-        <Text style={styles.headerText}>Nie przegap szansy na zdobycie atrakcyjnych premii w trakcie realizacji swoich celów inwestycyjnych. Już dziś skonfiguruj zlecenie stałe i obserwuj, jak rosną Twoje oszczędności!</Text>
-
-
+        <Image
+          source={require('./2.png')}
+          style={{ width: 120, height: 120, position: 'absolute', top: 10, right: 10 }}
+        />
+        <Text style={[styles.headerText, { marginRight: 140 }]}>
+        Masz szansę zarobić aż do 200 zł!
+        </Text>
+        <Text style={[styles.headerText2, { marginRight: 130 }]}>
+        Zakładając zlecenie stałe na swoje inwestycje, możesz otrzymać premie w wysokości do 200 zł
+        </Text>
+        <Text style={[styles.headerText2, { marginRight: 50 }]}>
+        Im większe jest Twoje zlecenie stałe, tym wyższy bonus możesz otrzymać.
+        </Text>
+        <Text style={[styles.headerText3]}>
+        Nawet do 200 zł!
+        </Text>
+        <Text style={styles.text}>Bonusy zostaną dodane do Twojego konta zgodnie z wybraną kwotą zlecenia stałego.</Text>
+        <Text style={styles.text}>Promocja jest dostępna przez ograniczony czas, więc nie przegap okazji, aby zmaksymalizować swoje nagrody.</Text>
+        <Text style={[styles.headerText4]}>
+        O więcej szczegółów zapytaj w punkcie banku
+        </Text>
       </View>
-
-     
-
     </View>
   );
 };
@@ -38,30 +38,55 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     textAlign: 'left',
   },
+  text: {
+    fontSize: 15, 
+    marginBottom: 5,
+    marginLeft: 15,
+    marginRight: 15,
+  
+  },
   headerText: {
-    fontSize: 15,
+    fontSize: 20,
     fontWeight: 'bold',
-    marginTop: 10, 
-    marginBottom: 5, 
-    marginLeft: 10, 
+    marginTop: 15,
+    marginBottom: 5,
+    marginLeft: 15,
     marginRight: 10,
     textAlign: 'left',
-
   },
-  text: {
-    fontSize: 12, 
+  headerText2: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 15,
     marginBottom: 5,
-    
-  
+    marginLeft: 15,
+    marginRight: 10,
+    textAlign: 'left',
+  },
+  headerText3: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    marginTop: 15,
+    marginBottom: 15,
+    fontStyle: 'italic',
+    textAlign: 'center',
+  },
+  headerText4: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 35,
+    marginBottom: 15,
+    marginLeft: 15,
+    marginRight: 10,
+    textAlign: 'center',
   },
   rectangle: {
     backgroundColor: 'white',
-    width: 350, 
-    height: 580, 
+    width: 350,
+    height: 580,
     borderRadius: 8,
     marginVertical: 20,
   },
-
 });
 
-export default OfferSecond;
+export default OfferFirst;
