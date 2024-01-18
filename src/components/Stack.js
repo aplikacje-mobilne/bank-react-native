65781f19c41d3da4dbd6c79cce1738ebccb0cd43
nutrilit <84638097+nutrilit@users.noreply.github.com>
2024-01-18 13:16:55 +0100

    Edit8 - biometria,pin,logout

--- src/components/Stack.js
+++ src/components/Stack.js
@@ -13,6 +13,7 @@ import Edit4 from '../screens/Edit4';
 import Edit5 from '../screens/Edit5';
 import Edit6 from '../screens/Edit6';
 import Edit7 from '../screens/Edit7';
+import Edit8 from '../screens/Edit8';
 import Payment1 from '../screens/Payment1';
 import Payment2 from '../screens/Payment2';
 import Start from '../screens/Start';
@@ -58,6 +59,7 @@ const StackNav = () => {
                   <Stack.Screen name="Edit5" children={() => <Edit5 setIsLoggedIn={setIsLoggedIn} />} options={{ title: 'Log Out' }} />
                   <Stack.Screen name="Edit6" children={() => <Edit6 setIsLoggedIn={setIsLoggedIn} />} options={{ title: 'Change PIN' }} />
                   <Stack.Screen name="Edit7" children={() => <Edit7 setIsLoggedIn={setIsLoggedIn} />} options={{ title: 'Change Account Password' }} />
+                  <Stack.Screen name="Edit8" children={() => <Edit8 setIsLoggedIn={setIsLoggedIn} />} options={{ title: 'Set Biometrics' }} />
                   <Stack.Screen name="Payment1" component={Payment1} options={{ title: 'Transfer' }} />
                   <Stack.Screen name="Payment2" component={Payment2} options={{ title: 'Transfer' }} />
                   <Stack.Screen name="TakePhoto" component={TakePhoto} />
